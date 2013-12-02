e21de8b6fe445fe202702866f0a0f75730fc43ff
roy sun <onecoders@gmail.com>
2013-12-02 09:31:22 +0800

    wechat public platfom materials

---
+++ Wechat public platfrom/about.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<!--
+Design by Free Responsive Templates
+http://www.free-responsive-templates.com
+Released for free under a Creative Commons Attribution 3.0 Unported License (CC BY 3.0) 
+-->
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Free Responsive Template #11 - ABC</title>
+<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Belgrano' rel='stylesheet' type='text/css'>
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="wrapper">
+	<header>
+    	<!--<div class="logo"><h1><a href="">Abc</a></h1></div>
+        <nav>
+            <ul id="navlist">
+                <li id="active"><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Customers</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="clearfloat"></div>-->
+        <img src="http://www.chinaunicom.com.cn/images/banner_about.jpg">
+    </header>
+    <section>
+    	<div class="horSeparator"></div>
+    	<h1>关于中国联通</h1>
+        <p>中国联合网络通信集团有限公司（简称“中国联通”）于2009年1月6日在原中国网通和原中国联通的基础上合并组建而成，在国内31个省（自治区、直辖市）和境外多个国家和地区设有分支机构，是中国唯一一家在纽约、香港、上海三地同时上市的电信运营企业，连续多年入选“世界500强企业”。</p>
+        <p>中国联通主要经营固定通信业务，移动通信业务，国内、国际通信设施服务业务，卫星国际专线业务、数据通信业务、网络接入业务和各类电信增值业务，与通信信息业务相关的系统集成业务等。中国联通于2009年4月28日推出全新的全业务品牌“沃”，承载了联通始终如一坚持创新的服务理念，为个人客户、家庭客户、集团客户提供全面支持。</p>
+        <p>近年来，中国联通的资产、人员、用户和收入规模明显扩大，企业综合实力得到明显提升。截至2012年底，中国联通资产规模达到5760.72亿元人民币，全系统从业人员29.48万人。截至2013年3月底，用户总数4.03亿户，其中移动电话用户25,070.8万户（3G用户8,781.6万户），宽带用户6,091.3万户，固定电话（含无线市话）用户9,127.5万户。中国联通的收入规模、用户规模及上市公司市值均位居全球电信运营商的前列。</p>
+        <p>中国联通拥有覆盖全国、通达世界的现代通信网络，积极推进固定网络和移动网络的宽带化，为广大用户提供全方位、高品质信息通信服务。2009年1月，中国联通获得了当今世界上技术最为成熟、应用最为广泛、产业链最为完善的WCDMA制式的3G牌照。在短短几个月的时间里，中国联通便建成了全球规模最大的WCDMA网络。目前，3G网络已经覆盖了全国所有城市。</p>
+        <p>面对全球电信业创新转型和我国深入推进信息化与工业化融合带来的新机遇和新挑战，中国联通将继续推进企业的创新发展进程，以全业务经营和3G发展为引擎，坚持以用户为中心，加强技术、业务、服务和管理创新，不断提升综合实力和核心竞争力，全面满足广大用户的信息服务需求，致力于成为信息生活的创新服务领导者，在国民经济和社会信息化进程中发挥主力军作用。</p>
+        <p>&nbsp;</p>
+        <div class="horSeparator"></div>
+    </section>
+    
+<footer>
+	<p>
+        Copyright &copy; Your Company Name. All rights reserved. Designed by <a href="http://www.free-responsive-templates.com" title="free responsive templates">Free Responsive Templates</a>, Validation 
+        <a class="footerLink" href="http://validator.w3.org/check/referer" title="This page validates as HTML5"><abbr title="HyperText Markup Language">HTML5</abbr></a> | 
+        <a class="footerLink" href="http://jigsaw.w3.org/css-validator/check/referer" title="This page validates as CSS"><abbr title="Cascading Style Sheets">CSS3</abbr></a>
+    </p>
+</footer>
+</body>
+</html>
